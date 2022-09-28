4b344b707b5d24a1f40a8e005ff9dc0bf4960961
th9716 <55468263+th9716@users.noreply.github.com>
2022-09-28 23:36:18 +0800

    Password

--- Frm_0919/Frm_M32_String.Designer.cs
+++ Frm_0919/Frm_M32_String.Designer.cs
@@ -36,6 +36,15 @@
             this.timerTime = new System.Windows.Forms.Timer(this.components);
             this.labTime2 = new System.Windows.Forms.Label();
             this.labTime3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_Password = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_ID = new System.Windows.Forms.TextBox();
+            this.labPassword = new System.Windows.Forms.Label();
+            this.labID = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.timerPassword = new System.Windows.Forms.Timer(this.components);
+            this.timerID = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // button1
@@ -100,11 +109,94 @@
             this.labTime3.TabIndex = 1;
             this.labTime3.Text = "label1";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(350, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "密碼:";
+            // 
+            // txt_Password
+            // 
+            this.txt_Password.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txt_Password.Location = new System.Drawing.Point(420, 63);
+            this.txt_Password.Name = "txt_Password";
+            this.txt_Password.Size = new System.Drawing.Size(143, 33);
+            this.txt_Password.TabIndex = 3;
+            this.txt_Password.TextChanged += new System.EventHandler(this.txt_Password_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(293, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "身分證字號:";
+            // 
+            // txt_ID
+            // 
+            this.txt_ID.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txt_ID.Location = new System.Drawing.Point(420, 115);
+            this.txt_ID.Name = "txt_ID";
+            this.txt_ID.Size = new System.Drawing.Size(143, 33);
+            this.txt_ID.TabIndex = 3;
+            this.txt_ID.TextChanged += new System.EventHandler(this.txt_ID_TextChanged);
+            // 
+            // labPassword
+            // 
+            this.labPassword.AutoSize = true;
+            this.labPassword.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labPassword.Location = new System.Drawing.Point(580, 72);
+            this.labPassword.Name = "labPassword";
+            this.labPassword.Size = new System.Drawing.Size(48, 24);
+            this.labPassword.TabIndex = 2;
+            this.labPassword.Text = "密碼";
+            // 
+            // labID
+            // 
+            this.labID.AutoSize = true;
+            this.labID.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labID.Location = new System.Drawing.Point(580, 124);
+            this.labID.Name = "labID";
+            this.labID.Size = new System.Drawing.Size(105, 24);
+            this.labID.TabIndex = 2;
+            this.labID.Text = "身分證字號";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(439, 198);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(113, 59);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "驗證";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // timerPassword
+            // 
+            this.timerPassword.Tick += new System.EventHandler(this.timerPassword_Tick);
+            // 
+            // timerID
+            // 
+            this.timerID.Tick += new System.EventHandler(this.timerID_Tick);
+            // 
             // Frm_M32_String
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.txt_ID);
+            this.Controls.Add(this.labID);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_Password);
+            this.Controls.Add(this.labPassword);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.labTime3);
             this.Controls.Add(this.labTime2);
             this.Controls.Add(this.labTime);
@@ -127,5 +219,14 @@
         private System.Windows.Forms.Timer timerTime;
         private System.Windows.Forms.Label labTime2;
         private System.Windows.Forms.Label labTime3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_Password;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_ID;
+        private System.Windows.Forms.Label labPassword;
+        private System.Windows.Forms.Label labID;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Timer timerPassword;
+        private System.Windows.Forms.Timer timerID;
     }
 }
--- Frm_0919/Frm_M32_String.cs
+++ Frm_0919/Frm_M32_String.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -58,5 +59,97 @@ namespace Frm_0919
             labTime3.Text = DateTime.Now.ToShortDateString();
             labTime.Text = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss");
         }
+
+
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            labPassword.Text = isCorrectPassword(txt_Password.Text) ? "密碼格式正確" : "密碼格式錯誤";
+            labID.Text = isCorrectID(txt_ID.Text) ? "格式正確" : "格式錯誤";
+        }
+
+        bool isCorrectPassword(string Password)
+        {
+            return Regex.IsMatch(Password, @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,16}$"); //Regex開頭^結尾$
+            //(?=.*)任意位置
+        }
+
+        bool isCorrectID(string ID)
+        {
+            return Regex.IsMatch(ID, @"^[A-Z]{1}[1-2]{1}[0-9]{8}$"); //[]限定位置 {}佔幾格
+        }
+
+        private void txt_Password_TextChanged(object sender, EventArgs e)
+        {
+            //labPassword.Text = isCorrectPassword(txt_Password.Text) ? "密碼格式正確" : "密碼格式錯誤";
+            if (isCorrectPassword(txt_Password.Text))
+            {
+                labPassword.Text = "密碼格式正確";
+                timerPassword.Enabled = false;
+            }
+            else
+            {
+                labPassword.Text = "密碼格式錯誤";
+                timerPassword.Enabled=true;
+            }
+
+
+        }
+
+        private void txt_ID_TextChanged(object sender, EventArgs e)
+        {
+            //labID.Text = isCorrectID(txt_ID.Text) ? "格式正確" : "格式錯誤";
+            if (isCorrectID(txt_ID.Text))
+            {
+                labID.Text = "格式正確";
+                timerID.Enabled = false;
+
+            }
+            else
+            {
+                labID.Text = "格式錯誤";
+                timerID.Enabled = true;
+
+            }
+
+        }
+
+        bool flag = true;
+        bool flag2 = true;
+        private void timerPassword_Tick(object sender, EventArgs e)
+        {
+            if (flag)
+            {
+                labPassword.BackColor = Color.Yellow;
+                labPassword.ForeColor = Color.Red;
+            }
+            else
+            {
+                labPassword.BackColor = Color.Red;
+                labPassword.ForeColor = Color.Yellow;
+            }          
+            flag = !flag;
+        }
+
+
+        private void timerID_Tick(object sender, EventArgs e)
+        {
+            if (flag2)
+            {
+                labID.BackColor = Color.Yellow;
+                labID.ForeColor = Color.Red;
+            }
+            else
+            {
+                labID.BackColor = Color.Red;
+                labID.ForeColor = Color.Yellow;
+            }
+            flag2 = !flag2;
+        }
+
+
+
+        
     }
 }
